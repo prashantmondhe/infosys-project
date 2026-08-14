c7230f4081d8b4796647c850e894a2fbb90f5f75
prashantmondhe <mondheprashant@gmail.com>
2026-08-14 18:49:57 +0530

    Create route.js

---
+++ app/api/register/route.js
@@ -0,0 +1,27 @@
+import dbConnect from '@/lib/mongodb';
+import User from '@/models/User';
+import bcrypt from 'bcryptjs';
+import { NextResponse } from 'next/server';
+
+export async function POST(request) {
+  try {
+    await dbConnect();
+    const { name, email, password } = await request.json();
+
+    
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json({ message: 'User already exists!' }, { status: 400 });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+   
+    const newUser = new User({ name, email, password: hashedPassword });
+    await newUser.save();
+
+    return NextResponse.json({ message: 'User registered successfully!' }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ message: 'Server error', error: error.message }, { status: 500 });
+  }
+}
