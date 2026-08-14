dac31f9d609122ed2bea9d381136a5405b0a0db7
prashantmondhe <mondheprashant@gmail.com>
2026-08-14 21:02:29 +0530

    Update route.js

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -3,12 +3,11 @@ import User from '@/models/User';
 import bcrypt from 'bcryptjs';
 import { NextResponse } from 'next/server';
 
-export async function POST(request) {
+export async function POST(req) {
   try {
     await dbConnect();
-    const { name, email, password } = await request.json();
+    const { name, email, password } = await req.json();
 
-    
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return NextResponse.json({ message: 'User already exists!' }, { status: 400 });
@@ -16,9 +15,11 @@ export async function POST(request) {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-   
-    const newUser = new User({ name, email, password: hashedPassword });
-    await newUser.save();
+    await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
 
     return NextResponse.json({ message: 'User registered successfully!' }, { status: 201 });
   } catch (error) {
